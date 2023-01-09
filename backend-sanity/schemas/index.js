6c2b24e03e1735936321b474852e391ca1a94b7c
Salman <sa1man4live002@gmail.com>
2023-01-09 22:53:29 +0300

    initialized react file structure with container and components

--- backend-sanity/schemas/index.js
+++ backend-sanity/schemas/index.js
@@ -1,2 +1,13 @@
+//imported schemas
 import testimonials from "./testimonials"
-export const schemaTypes = [testimonials]
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+// Schemas given to builder to provide result in Sanity
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
